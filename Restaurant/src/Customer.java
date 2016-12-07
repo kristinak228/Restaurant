cc2fafb3d528cb9478890be327d28d3c29f84fda
Kristina Kolibab <tinytina228@yahoo.com>
2016-12-06 20:43:29 -0500

    Update Customer.java

--- Restaurant/src/Customer.java
+++ Restaurant/src/Customer.java
@@ -1,7 +1,6 @@
 import java.util.Random;
 
 /**
- *
  * @author Kristina Kolibab
  */
 public class Customer implements Runnable {
@@ -12,7 +11,6 @@ public class Customer implements Runnable {
     private String customerName; //name of this customer
     
     //Constructor
-    //initializes data
     public Customer(Table table, String customerName){
         this.table = table;
         this.customerName = customerName;
@@ -22,5 +20,24 @@ public class Customer implements Runnable {
     @Override
     public void run(){
         Random rand = new Random();
+        //customer is ready to order, they call the eat() method
+        String currentDish = table.eat();
+        
+        //test
+        System.out.println("Dish being passed through is: " + currentDish);
+        System.out.println("Current customer being served: " + customerName);
+        
+        //customer has to wait for the waiter to prepare the dish
+        while(!currentDish.equals("Course Done")){ 
+            
+            System.out.println(customerName + " is eating: " + currentDish);
+            try{
+                Thread.sleep(rand.nextInt(MAX_CUSTOMER_MILLIS));
+            } catch (InterruptedException e){
+                System.err.println(e.getMessage());
+            }
+            currentDish = table.eat(); 
+        } //do i need to reset "course done" ?
+        
     }
 }
