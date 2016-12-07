f12ab5cbf3d0ee4b4b2ee11212b314e695cb67bc
Kristina Kolibab <tinytina228@yahoo.com>
2016-12-06 20:44:01 -0500

    Update Table.java

--- Restaurant/src/Table.java
+++ Restaurant/src/Table.java
@@ -4,19 +4,15 @@
  * and open the template in the editor.
  */
 
-/*
- *
+/**
  * @author Kristina Kolibab
  */
-public class Table {
-    //Data
+public class Table{
     private String course; //name of course
     //flag used to see if table is empty or not (has unfinished course)
     private boolean isEmpty; 
-    private Object obj; //synchronization object (not necessary)
     
-    //Constructor
-    //default
+    //Default constructor
     public Table(){
         isEmpty = true;
         course = null;
@@ -24,11 +20,30 @@ public class Table {
     
     //Methods
     //implements waiter serving a course
-    public synchronized void serve(){
-        //WAT
+    public synchronized void serve(String course){
+        while(isEmpty == false){ //waits for customer to finish eating
+            try{
+                wait();
+            } catch(InterruptedException e){
+                System.err.println(e.getMessage());
+            }
+        }
+        this.course = course; 
+        isEmpty = false; 
+        notifyAll();
     }
+        
     //implements customer eating course
     public synchronized String eat(){
+        while(isEmpty == true){ //waits for waiter to serve food
+            try{
+                wait();
+            } catch(InterruptedException e){
+                System.err.println(e.getMessage());
+            }
+        }
+        isEmpty = true; 
+        notifyAll(); //wakes up all threads waiting for a lock
         
         return course;
     }
